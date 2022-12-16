0f84b2fd5e50ccc4226e9d1671a952c3cb7d21e5
barmendel <bar1.mendel@gmail.com>
2022-12-16 08:31:05 +0200

    add logo header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,13 +1,13 @@
 import { NavLink } from 'react-router-dom'
-
+import logoAirbnb from '../assets/img/airbna-logo.png'
 export const AppHeader = () => {
   return (
     <section className="app-header main-layout full">
       <div className="layout-container">
-        <div>Logo</div>
+        <img className='logo-img' src={logoAirbnb} alt="" />
         <nav className="navbar">
           <NavLink end to="/" >Home</NavLink>
-          <NavLink to="/about">About</NavLink>
+          {/* <NavLink to="/about">About</NavLink> */}
         </nav>
       </div>
     </section>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,6 +2,7 @@ export const Home = () => {
 
     return (
         <section className="home">
+            <h1>hello from home</h1>
         </section>
     )
 }
